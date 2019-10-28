38948d4d71d037647c4ab5ffb6f2ef646b789758
tilak <tilak.panchal@hamstede.in>
2019-10-29 00:17:16 +0530

    setup initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "trailingComma": "all",
+  "semi": true,
+  "singleQuote": true,
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "tabWidth": 2,
+  "parser": "flow"
+};
---
+++ src/styles/index.js
@@ -0,0 +1,6 @@
+import { Colors } from 'react-native-ui-lib';
+import colors from './colors';
+
+Colors.loadColors(colors);
+
+export default colors;
